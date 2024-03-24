c0618e1e53a0b874059e6370b8327c83e69d2d2a
Dmitry Avdoshkin <works-dim@yandex.ru>
2024-03-24 23:13:06 +0800

    2-nd_course_HW-17_v1

---
+++ HW-17/api.js
@@ -0,0 +1,95 @@
+import { user } from "./index.js";
+import { sanitizeHtml } from "./sanitizeHtml.js";
+import _ from "lodash";
+
+const commentsUrl = "https://webdev-hw-api.vercel.app/api/v2/Dmitry-Avdoshkin/comments";
+const userUrL = "https://wedev-api.sky.pro/api/user/login";
+const newUserUrl = "https://wedev-api.sky.pro/api/user";
+
+export const setToken = () => {
+  const token = user ? `Bearer ${user.token}` : undefined;
+  return token;
+};
+
+export function getComments() {
+  return fetch(commentsUrl, {
+    method: "GET",
+    headers: {
+      Authorization: setToken(),
+    },
+  }).then((response) => {
+    return response.json();
+  });
+}
+
+export function postComment(name, text) {
+  return fetch(commentsUrl, {
+    method: "POST",
+    headers: {
+      Authorization: setToken(),
+    },
+    body: JSON.stringify({
+      // name: name,
+      text: sanitizeHtml(text),
+      // forceError: true,
+    }),
+  }).then((response) => {
+    return response.json();
+  });
+}
+
+export function deleteComment({ id }) {
+  return fetch(`${commentsUrl}/${id}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: setToken(),
+    },
+  }).then((response) => {
+    return response.json();
+  });
+}
+
+export function likeComment({ id }) {
+  // console.log(likeComment);
+  return fetch(`${commentsUrl}/${id}/toggle-like`, {
+    method: "POST",
+    headers: {
+      Authorization: setToken(),
+    },
+  }).then((response) => {
+    return response.json();
+  });
+}
+
+export function login({ login, password }) {
+  return fetch(userUrL, {
+    method: "POST",
+    body: JSON.stringify({
+      login,
+      password,
+      // forceError: true,
+    }),
+  }).then((response) => {
+    if (response.status === 400) {
+      throw new Error("Неверный запрос");
+    }
+    return response.json();
+  });
+}
+
+export function registration({ login, name, password }) {
+  return fetch(newUserUrl, {
+    method: "POST",
+    body: JSON.stringify({
+      login,
+      name: _.capitalize(name),
+      password,
+      // forceError: true,
+    }),
+  }).then((response) => {
+    if (response.status === 400) {
+      throw new Error("Неверный запрос");
+    }
+    return response.json();
+  });
+}
---
+++ HW-17/index.js
@@ -0,0 +1,50 @@
+//use strict";
+import { getComments } from "./api.js";
+import { renderComments } from "./render.js";
+import { setToken } from "./api.js";
+import { format } from "date-fns";
+import {
+  // getFromLocalStorage,
+  saveToLocalStorage,
+  removeFromLocalStorage,
+} from "./helpers.js";
+let comments = [];
+
+// Форматируем дату из библиотеки date-fns
+const now = new Date();
+const createDate =
+  // format(now, "dd/MM/yyyy hh:mm"); // 20/03/2024 10:33
+  // format(now, "MM-dd-yyyy hh:mm"); // 21-26-2024 10:33
+  // format(now, "dd.MM.yyyy hh:mm:ss"); // 21.03.2024 10:33:41
+  format(now, "yyyy-MM-dd hh.mm.ss"); // 2024-03-24 10.33.41
+
+// export let user = getFromLocalStorage();
+export let user = null;
+export const setUser = (newUser) => {
+  user = newUser;
+  saveToLocalStorage(user);
+};
+
+export const logout = () => {
+  user = null;
+  removeFromLocalStorage();
+};
+// Запрос двнных в API на комментарий
+export const fetchAndRenderComments = (comments) => {
+  getComments({ token: setToken() })
+    .then((responseData) => {
+    const appComments = responseData.comments.map((comment) => {
+      return {
+        id: comment.id,
+        name: comment.author.name,
+        date: createDate,
+        text: comment.text,
+        likes: comment.likes,
+        isLiked: comment.isLiked,
+      };
+    });
+    comments = appComments;
+    renderComments(comments);
+  });
+};
+fetchAndRenderComments(comments);
